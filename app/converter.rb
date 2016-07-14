7df87b3bf1fcb8ab93765c5bba947090039fc232
Elan Qisthi <elanqisthi@gmail.com>
2016-07-14 17:29:08 +0700

    add text fitter method

--- app/converter.rb
+++ app/converter.rb
@@ -7,14 +7,18 @@ module App
       @size = size
       link = "#{ENV['UNSPLASH_WEB']}/#{size}/#{size}/?random"
       @image = Image.read(link).first
+      @orig_image = @image.dup
     end
 
     def place_text(text)
       # draw object, width, height, x, y, text
       txt = Draw.new
-      @image.annotate(txt, 0, 0, 0, 0, text) do
+      text = fit_text(text, 1200)
+      @image.annotate(txt, 0, 0, 0, @size / 8, text) do
+        txt.font_family 'helvetica'
+        txt.font_weight = 300
         txt.gravity = Magick::NorthGravity
-        txt.pointsize = 70
+        txt.pointsize = 80
         txt.fill = '#ffffff'
       end
       self
@@ -35,8 +39,51 @@ module App
       @image = image
     end
 
-    def image
-      @image
+    def original_image
+      @orig_image
     end
+
+    private
+
+    def text_fit?(text, width)
+      tmp_image = Image.new(width, 1200)
+      drawing = Draw.new
+      drawing.annotate(tmp_image, 0, 0, 0, 0, text) do |txt|
+        txt.font_family 'helvetica'
+        txt.font_weight = 300
+        txt.gravity = Magick::NorthGravity
+        txt.pointsize = 80
+      end
+      metrics = drawing.get_multiline_type_metrics(tmp_image, text)
+      (metrics.width < width)
+    end
+
+    def fit_text(text, width)
+      separator = ' '
+      line = ''
+
+      unless text_fit?(text, width) && text.include?(separator)
+        i = 0
+        text.split(separator).each do |word|
+          tmp_line = i == 0 ? line + word : line + separator + word
+
+          if text_fit?(tmp_line, width)
+            unless i == 0
+              line += separator
+            end
+            line += word
+          else
+            unless i == 0
+              line +=  '\n'
+            end
+            line += word
+          end
+          i += 1
+        end
+        text = line
+      end
+      text
+    end
+
   end
 end
--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@ require_relative 'app/app.rb'
 
 Dotenv.load
 converter = App::Converter.new
-default_image = converter.image
-converter.place_text('testing')
+default_image = converter.original_image
+converter.place_text('Sekarang pakai text yang sangat panjang sekali kira-kira overlap gak ya')
          .write_to('/Volumes/EQ-DOCS/test-folder/generated_image.jpg')
 
 filtered_converter = App::Converter.new
